5058a166f0a15cee6e27f4507711a778f6a58cec
animesh3640 <127398605+animesh3640@users.noreply.github.com>
2023-06-18 07:26:19 +0000

    f

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,31 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [gender,setGender] = useState('');
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h2>Select Your Gender</h2>
+        <input type="radio" name="gender" value="male" onChange={()=>{setGender='male'}}/>
+        <input type="radio" name="gender" value="female" onChange={()=>{setGender='female'}}/>
+        <h2>Select Your Shirt Size</h2>
+       {
+        (gender=='male')&&(
+          <select name="opt" id="opt">
+          <option value="large">Large</option>
+          <option value="medium">Mediam</option>
+          <option value="Small">Small</option>
+        </select>
+        )
+       }
+        {
+          (gender=='female')&&(
+            <select name="opt" id="opt">
+              <option value="large">Large</option>
+              <option value="medium">Mediam</option>
+              <option value="Small">Small</option>
+            </select>
+          )
+        }
     </div>
   )
 }
